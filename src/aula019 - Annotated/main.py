0aa6ebff3d9c213fa0c2e8657a29e94572a987d5
gaaiuss <caio.gui.castro@gmail.com>
2026-08-17 14:04:48 -0300

    add: initial annotated

---
+++ src/aula019 - Annotated/main.py
@@ -0,0 +1,7 @@
+"""Annotated
+
+Metadata: Data that describes other data. Like images description (image metadata),
+python modules, functions, classes, etc.
+
+Annotated: Parametrized type used to add metadata on another types.
+"""
